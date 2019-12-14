c832946881e76ef0da45e3024407b63d536f9cac
Davies Maina <daviesmaina@outlook.com>
2019-12-14 18:54:39 +0300

    fixed bug in notif sound

--- resources/assets/js/components/Notification.vue
+++ resources/assets/js/components/Notification.vue
@@ -55,7 +55,7 @@ export default {
 
         playSound(){
             let notifAlert=new Audio(this.sound);
-            alert.play();
+            notifAlert.play();
 
         },
         getNotifications(){
